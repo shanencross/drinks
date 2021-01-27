121d3ded72192eabf117339a6fd125f3ef1d4978
Shanen Cross <shanencross@yahoo.com>
2021-01-26 19:58:13 -0800

    Change to confirm rather than prompt
    Co-authored-by: Julian Leonardo <louderthanbombs9@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,9 @@
 $(document).ready(function() {
-  const age = parseInt(prompt("How old are you?"));
+	const over21 = confirm("Are you over 21? Click OK for yes or Cancel for no.");
 
-  if (age > 21) {
-    $('#drinks').show();
-	} else if (age === 21) {
-		alert("Now don't go wild!");
+	if (over21) {
 		$('#drinks').show();
 	} else {
-    $('#under-21').show();
-  }
+		$('#under-21').show();
+	}
 });
