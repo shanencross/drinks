6446be03aa78b09afaadd977b9bb2a7b1790391b
Shanen Cross <shanencross@yahoo.com>
2021-01-26 19:50:03 -0800

    Fix >= to be ===
    Co-authored-by: Julian Leonardo <louderthanbombs9@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
   const age = parseInt(prompt("How old are you?"));
 
-  if (age >= 21) {
+  if (age > 21) {
     $('#drinks').show();
 	} else if (age === 21) {
 		alert("Now don't go wild!");
